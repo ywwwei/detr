c85a579cc9d690b16eb9df172da036d23c4cb18d
ywwwei <wei96@wisc.edu>
2022-02-14 21:49:05 -0600

    Add argoverseHD dataset class

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,6 +4,7 @@ import torchvision
 
 from .coco import build as build_coco
 from .ytvis import build as build_ytvis
+from .argoversehd import build as build_argoverse
 
 
 def get_coco_api_from_dataset(dataset):
@@ -27,4 +28,6 @@ def build_dataset(image_set, args):
         return build_coco_panoptic(image_set, args)
     if args.dataset_file == 'ytvis':
         return build_ytvis(image_set, args)
+    if args.dataset_file == 'argoverse':
+        return build_argoverse(image_set, args)
     raise ValueError(f'dataset {args.dataset_file} not supported')
